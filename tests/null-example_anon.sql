cee3c2e4f961eaf1d092b6d4ff59fb8cf2eb38ae
Pierre POMES <pierre.pomes@gmail.com>
2022-04-17 17:07:17 -0400

    Add basic tests target

---
+++ tests/null-example_anon.sql
@@ -0,0 +1,62 @@
+-- MySQL dump 10.13  Distrib 5.6.51, for Linux (x86_64)
+--
+-- Host: 127.0.0.1    Database: test
+-- ------------------------------------------------------
+-- Server version	5.6.51
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Current Database: `test`
+--
+
+CREATE DATABASE /*!32312 IF NOT EXISTS*/ `test` /*!40100 DEFAULT CHARACTER SET latin1 */;
+
+USE `test`;
+
+--
+-- Table structure for table `testing`
+--
+
+DROP TABLE IF EXISTS `testing`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `testing` ( 
+  `colA` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `colB` varchar(254) CHARACTER SET utf8 COLLATE utf8_unicode_ci DEFAULT NULL,
+  `colC` varchar(254) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  PRIMARY KEY (`colA`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `testing`
+--
+
+LOCK TABLES `testing` WRITE;
+/*!40000 ALTER TABLE `testing` DISABLE KEYS */;
+set autocommit=0;
+INSERT INTO `testing` VALUES (1,'btvwfnjpzxtilmgqopsiwngvckkfciei','yhngvcxcwieiuyrnoxfkppwamxfgcjwf'),(2,'sjpenzdnyemtwjowggpsoehmjolpdqkd','msywcmwklrbevepaworpasuxixouzllz'),(3,NULL,'rubenemyxsdonnubpliccresenxcrdxo'),(4,'sqcgairgjcmloxljoaeaqsxoieysolho','wqciibfxylriupvtblmndiuicnjgkcbb'),(5,NULL,'reewtklnkyytprrkhavzqkatmzkchrod'),(6,'tbiodcgvpmvlrxhowiclemtvdxclikdt','bhkbkwhehsvtyuwpuycuyhnvssdjjvdx');
+/*!40000 ALTER TABLE `testing` ENABLE KEYS */;
+UNLOCK TABLES;
+commit;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2022-04-12 20:06:16
