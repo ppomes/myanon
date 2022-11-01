55ec1d266b4dc19eafd50d9f98ac2521d3b82434
Pierre POMES <pierre.pomes@gmail.com>
2022-11-01 18:40:39 -0400

    Add support for POINT and GEOMETRY types

--- tests/test1.sql
+++ tests/test1.sql
@@ -1,8 +1,8 @@
--- MySQL dump 10.13  Distrib 8.0.29, for Linux (aarch64)
+-- MySQL dump 10.13  Distrib 8.0.31, for Linux (aarch64)
 --
 -- Host: localhost    Database: test1
 -- ------------------------------------------------------
--- Server version	8.0.29-0ubuntu0.22.04.2
+-- Server version	8.0.31-0ubuntu0.22.04.1
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -49,7 +49,9 @@ CREATE TABLE `lottypes` (
   `text4` longtext,
   `enum1` enum('small','meduim','large') DEFAULT NULL,
   `json1` json DEFAULT NULL,
-  `set1` set('C','Rust','Haskell') DEFAULT NULL
+  `set1` set('C','Rust','Haskell') DEFAULT NULL,
+  `point1` point /*!80003 SRID 0 */ DEFAULT NULL,
+  `geometry1` geometry DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -59,7 +61,7 @@ CREATE TABLE `lottypes` (
 
 LOCK TABLES `lottypes` WRITE;
 /*!40000 ALTER TABLE `lottypes` DISABLE KEYS */;
-INSERT INTO `lottypes` VALUES (1,2,3,4,5,6,5.20,1e17,2e16,'2021-01-20 15:00:00','2021-01-20','2021-02-14 15:41:45','a','b',_binary '\0\0\0xœ3\0\02\02\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0',_binary '\0\0\0xœ3\0\03\03',_binary '\0\0\0xœ3\0\04\04',_binary '\0\0\0xœ3\0\05\05',_binary '\0\0\0xœ3\0\06\06',_binary '\0\0\0xœ3\0\07\07','text1','text2','text3','text4','small','{\"key\": \"value\"}','C,Haskell');
+INSERT INTO `lottypes` VALUES (1,2,3,4,5,6,5.20,1e17,2e16,'2021-01-20 15:00:00','2021-01-20','2021-02-14 15:41:45','a','b',_binary '\0\0\0xœ3\0\02\02\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0',_binary '\0\0\0xœ3\0\03\03',_binary '\0\0\0xœ3\0\04\04',_binary '\0\0\0xœ3\0\05\05',_binary '\0\0\0xœ3\0\06\06',_binary '\0\0\0xœ3\0\07\07','text1','text2','text3','text4','small','{\"key\": \"value\"}','C,Haskell',_binary '\0\0\0\0\0\0\0\0\0\0\0\0\0\ð?\0\0\0\0\0\0\ð?',_binary '\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0@\0\0\0\0\0\0\0@\0\0\0\0\0\0@\0\0\0\0\0\0\0@\0\0\0\0\0\0@\0\0\0\0\0\0\0\0\0\0\0\0\0\0@\0\0\0\0\0\0\0\0\0\0\0\0\0\0@');
 /*!40000 ALTER TABLE `lottypes` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -132,6 +134,29 @@ INSERT INTO `tata` VALUES (-1,'pp@mydomain.com');
 UNLOCK TABLES;
 
 --
+-- Table structure for table `test_with_column_names`
+--
+
+DROP TABLE IF EXISTS `test_with_column_names`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `test_with_column_names` (
+  `a` int unsigned DEFAULT NULL,
+  `b` int unsigned DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `test_with_column_names`
+--
+
+LOCK TABLES `test_with_column_names` WRITE;
+/*!40000 ALTER TABLE `test_with_column_names` DISABLE KEYS */;
+INSERT INTO `test_with_column_names` VALUES (1,1),(1,1);
+/*!40000 ALTER TABLE `test_with_column_names` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `the_blobs`
 --
 
@@ -182,38 +207,6 @@ INSERT INTO `toto` VALUES (6128,'cpc','ppomes2','otherval1'),(2,'Ã Ã§Ãª','simon'
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
---
--- Table structure for table `test_with_column_names`
---
-
-DROP TABLE IF EXISTS `test_with_column_names`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `test_with_column_names` (
-  `a` int unsigned DEFAULT NULL,
-  `b` int unsigned DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `test_with_column_names`
---
-
--- Note: the following are not yet supported, as they are not possible from a direct `mysqldump`
--- Specifying a reduced number of columns, e.g.:
---   INSERT INTO `test_with_column_names` (`b`) VALUES (1);
---   INSERT INTO `test_with_column_names` (`a`) VALUES (1);
--- Specifying the columns in a different order, e.g.:
---   INSERT INTO `test_with_column_names` (`b`, `a`) VALUES (1, 1);
-
-LOCK TABLES `test_with_column_names` WRITE;
-/*!40000 ALTER TABLE `test_with_column_names` DISABLE KEYS */;
-INSERT INTO `test_with_column_names` (`a`, `b`) VALUES (1, 1);
-INSERT INTO `test_with_column_names` VALUES (1, 1);
-/*!40000 ALTER TABLE `test_with_column_names` ENABLE KEYS */;
-UNLOCK TABLES;
-
-
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
 /*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
@@ -222,4 +215,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2022-07-17 16:04:55
+-- Dump completed on 2022-11-01 18:36:07
