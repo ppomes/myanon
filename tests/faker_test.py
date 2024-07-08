564b03006820b1c8adea0f9d8efb18d0cfac36ff
Pierre POMES <pierre.pomes@gmail.com>
2024-07-08 15:59:33 -0400

    Issue #5 - Add a unused param to python faker test

--- tests/faker_test.py
+++ tests/faker_test.py
@@ -1,14 +1,14 @@
 from faker import Faker
 
-def email():
+def email(notused):
     fake = Faker()
     return fake.email()
 
-def firstname():
+def firstname(notused):
     fake = Faker()
     return fake.first_name()
 
-def lastname():
+def lastname(notused):
     fake = Faker()
     return fake.last_name()
 
